5255415b9152f35f03b948f3e990a7bad93b1665
Blue Jessen <canealisblue@gmail.com>
2022-03-24 08:16:36 -0700

    Add meal test implementation

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodName) {
+var deliciousFood = 'Delicious ' +foodName;
+return deliciousFood;
 }
 
+function createMenuItem(foodDish, price, type) {
+  return { name: foodDish,
+    price: price,
+    type: type
+  };
+}
+//var testFood = createMenuItem('Burrito',13, 'Dinner');
+//console.log(testFood);
+
+function addIngredients(ingredients, ingredientList){
+  if (ingredientList.includes(ingredients)=== false){
+ingredientList.push(ingredients);
+} else{
+  console.log("Sorry that ingredient is already in the list");
+}
+}
+
+function formatPrice (price){
+  return "$" + price;
+}
+
+function decreasePrice(price){
+  return price * .90;
+}
+function createRecipe (title, ingredients, recipeType){
+  return {title: title,
+  ingredients: ingredients,
+type: recipeType };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
