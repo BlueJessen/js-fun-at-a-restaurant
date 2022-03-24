388c45db869e572dd03a56cbb3169ef8bae9dce8
Blue Jessen <canealisblue@gmail.com>
2022-03-24 13:17:04 -0700

    Create takeOrder and refundOrder function

--- src/order.js
+++ src/order.js
@@ -1,7 +1,21 @@
 
+function takeOrder(order, deliveryOrders){
+  if(order.orderType === "delivery"
+        && deliveryOrders.length < 3 ){
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(order,orderGroup){
+  for (var i=0; i < orderGroup.length; i++){
+    if(orderGroup[i].orderNumber === order){
+      orderGroup.splice(i,1);
+    }
+  }
+}
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
