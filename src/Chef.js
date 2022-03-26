5f4573d5f4dea8d94f0e3e2a428c62d7e9176952
Blue Jessen <canealisblue@gmail.com>
2022-03-26 13:44:58 -0700

    Update restaurant functions

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,15 @@
 class Chef {
-  constructor() {
-    
+  constructor(name) {
+this.name = name;
   }
 }
 
 module.exports = Chef;
+
+// function greetCustomer(name,morning){
+//   if (morning === true){
+//   return `Good morning, ${name}!`
+// }else{
+//   return `Hello, ${name}!`
+// }
+// }
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,50 @@
 
+function createRestaurant(restaurantInfo){
+  return restaurant = {name: restaurantInfo,
+                      menus: {breakfast:[],lunch:[], dinner:[]},
+                      };
+              }
+
+function addMenuItem(restaurant,menuItem){
+  //if menu item is breakfast
+  if (menuItem.type === "breakfast" &&
+          !restaurant.menus.breakfast.includes(menuItem)){
+    restaurant.menus.breakfast.push(menuItem);
+    //menu item is lunch
+  }else if (menuItem.type === "lunch" &&
+                !restaurant.menus.lunch.includes(menuItem)){
+        restaurant.menus.lunch.push(menuItem);
+        //menu item is dinner
+      }else if (menuItem.type === "dinner" &&
+                  !restaurant.menus.dinner.includes(menuItem)){
+          restaurant.menus.dinner.push(menuItem);
+  }
+  }
+//buggy V  need to figure out how to access the array within the an
+//object cleanly.
+// function removeMenuItem(restaurant,menuItem,type){
+//   for (var i=0; i<restaurant.menus.; i++){
+//     if(restaurant.menus.type[i].name === menuItem){
+//       restaurant.menus.type.splice(i,1);
+//     }
+//   }
+//   }
+
+function removeMenuItem(restaurant,itemName,type){
+  for(var i=0; i<restaurant.menus[type].length; i++){
+    if(restaurant.menus[type][i].name.includes(itemName)){
+      restaurant.menus[type].splice(i,1);
+      return `No one is eating our ${itemName} - it has been removed from the ${type} menu!`;
+    }
+  }
+      return `Sorry, we don't sell ${itemName}, try adding a new recipe!`;
+  }
+
+
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
