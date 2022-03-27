6995e1c5e9eff6edf421a34671f74d2b30a10da7
Blue Jessen <canealisblue@gmail.com>
2022-03-26 17:00:50 -0700

    Add chef class

--- src/Chef.js
+++ src/Chef.js
@@ -1,15 +1,24 @@
 class Chef {
-  constructor(name) {
+  constructor(name, restaurant) {
 this.name = name;
+this.restaurant = restaurant;
   }
+  //methods
+  greetCustomer(name, morning){
+    if (morning === true){
+    return `Good morning, ${name}!`
+  }
+    return `Hello, ${name}!`
+  }
+  checkForFood(foodItem){
+
+    if (restaurant.menus[foodItem.type].includes(foodItem)){
+return `Yes, we're serving ${foodItem.name} today!`;
+    }
+    return `Sorry, we aren't serving ${foodItem.name} today.`
+  }
+
 }
 
-module.exports = Chef;
 
-// function greetCustomer(name,morning){
-//   if (morning === true){
-//   return `Good morning, ${name}!`
-// }else{
-//   return `Hello, ${name}!`
-// }
-// }
+module.exports = Chef;
